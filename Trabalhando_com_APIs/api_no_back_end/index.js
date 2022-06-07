71f1f1fb846342248bc9f0ad8b964c2fc74af015
DyogoViana <dyogo.vianaa@gmail.com>
2022-06-07 18:15:08 -0300

    POST

--- Trabalhando_com_APIs/api_no_back_end/index.js
+++ Trabalhando_com_APIs/api_no_back_end/index.js
@@ -4,4 +4,11 @@ const app = express();
 app.listen('3000');
 console.log("=) estou te ouvindo.");
 
-app.route('/').get( (request, response) => response.send("Follow the white Rabbit"))
+
+// middleware
+app.use(express.json());
+
+app.route('/').post( (request, response) => response.send(request.body));
+
+// GET
+// app.route('/').get( (request, response) => response.send("Follow the white Rabbit"))
