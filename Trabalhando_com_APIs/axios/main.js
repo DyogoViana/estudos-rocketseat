4e54512136bcc3996a5f739979f932dbd0476dc2
DyogoViana <dyogo.vianaa@gmail.com>
2022-06-29 17:30:34 -0300

    [ POST ]

--- Trabalhando_com_APIs/axios/main.js
+++ Trabalhando_com_APIs/axios/main.js
@@ -1,5 +1,12 @@
 const url = "http://localhost:5500/api";
 
+const newUser = {
+    name: "Manu Ginobili",
+    avatar: "https://picsum.photos/200/300",
+    city: "Bahía Blanca, Argentina"
+}
+
+// GET
 function getUsers() {
     axios.get(url)
         .then(response => {
@@ -7,7 +14,17 @@ function getUsers() {
                 response.data
             )
         }) // quando dá certo.
-        .catch(error => console.log(error)); // quando dá erro.
+        .catch(error => console.error(error)); // quando dá erro.
 }
 
+// POST
+function addNewUser(newUser) {
+    axios.post(url, newUser)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => console.error(error))
+};
+
 getUsers();
+addNewUser(newUser);
