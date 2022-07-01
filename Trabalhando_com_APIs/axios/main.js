35259ab33b0951290c6ff2578a3e7097ffec963e
DyogoViana <dyogo.vianaa@gmail.com>
2022-07-01 16:12:31 -0300

    [ PUT ]

--- Trabalhando_com_APIs/axios/main.js
+++ Trabalhando_com_APIs/axios/main.js
@@ -1,5 +1,19 @@
 const url = "http://localhost:5500/api";
 
+// Novo usuário.
+const newUser = {
+    name: "Manu Ginobili",
+    avatar: "https://picsum.photos/200/300",
+    city: "Bahía Blanca, Argentina"
+}
+
+// Editar usuário.
+const userUpdated = {
+    name: "Caravaggio",
+    avatar: "https://picsum.photos/200/300",
+    city: "Porto Ercole, Itália"
+}
+
 
 // GET
 function getUsers() {
@@ -21,6 +35,7 @@ function addNewUser(newUser) {
         .catch(error => console.error(error))
     };
     
+// Pega um usuário.
 function getUser(id) {
     axios.get(`${url}/${id}`)
         .then(response => {
@@ -32,12 +47,14 @@ function getUser(id) {
         .catch(error => console.error(error))
 };
 
-getUsers();
-getUser(2);
-const newUser = {
-    name: "Manu Ginobili",
-    avatar: "https://picsum.photos/200/300",
-    city: "Bahía Blanca, Argentina"
+// Editar usuário.
+function updateUser(id, userUpdated) {
+    axios.put(`${url}/${id}`, userUpdated)
+        .then(response => console.log(response))
+        .catch(error => console.error(error))
 }
 
+getUsers();
+getUser();
 addNewUser(newUser);
+updateUser(4, userUpdated);
