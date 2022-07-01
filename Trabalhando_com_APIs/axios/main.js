4ceb5daf8bf3e1e827897b7cf45bd5aa54b181f0
DyogoViana <dyogo.vianaa@gmail.com>
2022-07-01 15:23:33 -0300

    GET com parametros

--- Trabalhando_com_APIs/axios/main.js
+++ Trabalhando_com_APIs/axios/main.js
@@ -1,17 +1,12 @@
 const url = "http://localhost:5500/api";
 
-const newUser = {
-    name: "Manu Ginobili",
-    avatar: "https://picsum.photos/200/300",
-    city: "Bahía Blanca, Argentina"
-}
 
 // GET
 function getUsers() {
     axios.get(url)
-        .then(response => {
-            apiResult.textContent = JSON.stringify(
-                response.data
+    .then(response => {
+        apiResult.textContent = JSON.stringify(
+            response.data
             )
         }) // quando dá certo.
         .catch(error => console.error(error)); // quando dá erro.
@@ -20,11 +15,29 @@ function getUsers() {
 // POST
 function addNewUser(newUser) {
     axios.post(url, newUser)
+    .then(response => {
+        console.log(response.data);
+        })
+        .catch(error => console.error(error))
+    };
+    
+function getUser(id) {
+    axios.get(`${url}/${id}`)
         .then(response => {
-            console.log(response.data);
+            userName.textContent = response.data.name
+            userCity.textContent = response.data.city
+            userID.textContent = response.data.id
+            userAvatar.src = response.data.avatar
         })
         .catch(error => console.error(error))
 };
 
 getUsers();
+getUser(2);
+const newUser = {
+    name: "Manu Ginobili",
+    avatar: "https://picsum.photos/200/300",
+    city: "Bahía Blanca, Argentina"
+}
+
 addNewUser(newUser);
